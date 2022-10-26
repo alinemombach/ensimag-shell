b2ece1ac314db0f5d85ee1cac4374a43528f0953
alinemombach <aline-mombach@hotmail.com>
2022-10-27 00:49:19 +0200

    until question5

--- src/ensishell.c
+++ src/ensishell.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "variante.h"
 #include "readcmd.h"
@@ -16,6 +17,15 @@
 #ifndef VARIANTE
 #error "Variante non défini !!"
 #endif
+
+typedef struct jobs {
+    pid_t pid;
+    char *cmd;
+    struct jobs *next;
+} jobs; 
+
+jobs *jobs_head = NULL; //list of jobs
+
 
 /* Guile (1.8 and 2.0) is auto-detected by cmake */
 /* To disable Scheme interpreter (Guile support), comment the
@@ -33,11 +43,10 @@ int question6_executer(char *line)
 	 * parsecmd, then fork+execvp, for a single command.
 	 * pipe and i/o redirection are not required.
 	 */
-	printf("Not implemented yet: can not execute %s\n", line);
-
-	/* Remove this line when using parsecmd as it will free it */
-	free(line);
-	
+	struct cmdline *l;
+    l = parsecmd(&line);
+    exec_commands(l);
+    
 	return 0;
 }
 
@@ -59,32 +68,117 @@ void terminate(char *line) {
 	exit(0);
 }
 
-void execute(struct cmdline *l){
-	pid_t pid = fork();
-	
-	switch (pid){
-	case -1:
-		perror("fork:");
-		break;
-	case 0:
-		//son
-		/*EXECVP
-		first argument: binary executable file that is a part of the PATH environment variable. This is an char* string.
-		second argument: presents the list of arguments to command. This is an array of char* strings.*/
-		execvp(l->seq[0][0], l->seq[0]);
-		
-		break;
-	default:
-		//father
+void add_jobs(pid_t pid, char *cmd){
+	//insert in front of the list
+	jobs *new_job = malloc(sizeof(jobs)); //allocate memory for the new job
+   	new_job->pid = pid;
 
-		break;
+   	 new_job->cmd = (char *) malloc(sizeof(cmd) + 1);
+   	 strcpy(new_job->cmd, cmd);
+    
+	new_job->next = jobs_head;
+    
+	jobs_head = new_job;
+
+}
+
+void print_jobs(){
+	if (jobs_head == NULL) {
+        printf("no background job running\n");
+        return;
+    }
+
+	else{
+		jobs *current = jobs_head;
+		while(current!= NULL){
+			printf("PID: %d Command: %s \n", current->pid, current->cmd);
+			current = current->next;
+		}
+	}
+}
+
+void delete_jobs(){
+	//delete jobs list
+	jobs *current = jobs_head;
+
+	while(jobs_head!=NULL){
+		current = jobs_head;
+		free(current);
+		jobs_head = jobs_head->next;
+	}
+}
+
+void exec_pipe(struct cmdline *line){
 	
+	int pipefd[2];	
+
+	pipe(pipefd);
+	
+	pid_t pid = fork();
+
+	switch(pid){
+	case -1:
+		perror("fork");
+	break;
+
+	case 0: //child
+		dup2(pipefd[1], 1);
+		close(pipefd[0]); 
+		close(pipefd[1]);
+		execvp(line->seq[0][0],line->seq[0]);
+	break;
+
+	default:
+		dup2(pipefd[0], 0); 
+		close(pipefd[1]); 
+		close(pipefd[0]);
+		execvp(line->seq[1][0], line->seq[1]);
+
+	break;
 	}
 }
 
 
+void execute(struct cmdline *line){
+	pid_t pid = fork();
+
+
+	switch (pid){
+	case -1:
+		perror("fork:");
+		break;
+		
+	case 0:
+		//child
+		if (line->seq[1]) //if there is a second command, so make a pipe
+			exec_pipe(line);
+		
+        if (strcmp(*line->seq[0], "jobs") == 0)
+            print_jobs();
+		
+		else
+			execvp(line->seq[0][0], line->seq[0]); //execute command
+		
+		break;
+		
+	default:
+	//parent
+		if(!line->bg){
+			delete_jobs();
+			int status;
+			waitpid(pid, &status, 0);
+		}
+		else{
+			add_jobs(pid, *line->seq[0]);
+		}
+	break;
+	}
+}
+
 int main() {
-        printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
+
+	
+		printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
 #if USE_GUILE == 1
         scm_init_guile();
@@ -139,20 +233,22 @@ int main() {
 			continue;
 		}
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
+		// if (l->in) printf("in: %s\n", l->in);
+		// if (l->out) printf("out: %s\n", l->out);
+		// if (l->bg) printf("background (&)\n");
 
 		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: oi", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
+		 for (i=0; l->seq[i]!=0; i++) {
+		 	char **cmd = l->seq[i];
+		 	printf("seq[%d]:", i);
+                         for (j=0; cmd[j]!=0; j++) {
+                                 printf("'%s' ", cmd[j]);
+                         }
+		 	printf("\n");
+		 }
 
-		execute(l);
+		/* Execute commands */
+        execute(l);
+		
 	}
 }
